ac16f18e1982fc1bcc80dcbf991420c8476f54ff
Anna Laban <laban.anya.a@gmail.com>
2025-06-24 15:31:52 +0300

    fix config file

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/testTask2/',
+  base: '/testTask_solidgate/',
 })
